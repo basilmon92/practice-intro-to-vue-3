aa836ed58f56a77886d6bf3eb75ceb6cc4d7ad21
Basil Kuriakose <bkuriakose@shoptrue.com>
2023-07-30 00:16:07 +0530

    completed L8

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            // image: './assets/images/socks_blue.jpg',
+            selectedVariant: 0,
+            // inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            onSale: true,
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -19,6 +21,23 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            return this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        brandOnSale() {
+            return this.brand + ' ' + this.product + ' is on sale'
         }
     }
 })
